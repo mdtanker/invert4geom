5b2da9d1853e93e1192bfd6034ccf6809914737a
mdtanker <matt.d.tankersley@gmail.com>
2023-11-22 13:14:43 +0100

    style: fixes style issues

--- src/invert4geom/__init__.py
+++ src/invert4geom/__init__.py
@@ -4,5 +4,6 @@ Copyright (c) 2023 Matt Tankersley. All rights reserved.
 invert4geom: Constrained gravity inversion to recover the geometry of a density
 contrast.
 """
+from __future__ import annotations
 
 __version__ = "0.1.0"
